0fd42333f6a8261b6beee4a1e594faf19c544737
czl0325 <295183917@qq.com>
2020-04-01 21:57:47 +0800

    1.0.2版本提交审核

--- hongdou-applet/miniprogram/pages/web-view/web-view.js
+++ hongdou-applet/miniprogram/pages/web-view/web-view.js
@@ -7,19 +7,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-    url: "https://mp.weixin.qq.com/s?__biz=MzA5ODE1NzMwNg==&mid=502812811&idx=1&sn=519f42e89d1055a65752ac6b202c4acc&chksm=0899271e3feeae080f635d8599368a4899f2949eb98a4d4084e6df6ec4062ecc02aa716bce8e#rd"
+    url: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // if (options.url) {
-    //   console.log(utils.urlDecode(options.url))
-    //   this.setData({
-    //     url: utils.urlDecode(options.url)
-    //   })
-    // }
+    if (options.url) {
+      console.log(utils.urlDecode(options.url))
+      this.setData({
+        url: utils.urlDecode(options.url)
+      })
+    }
   },
 
   /**
